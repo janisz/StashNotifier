8b69d01a5b411780a086222b7ef49b4451585fcb
Tomek Janiszewski <janiszt@gmail.com>
2014-07-19 13:32:34 +0200

    Fix sample

--- background.js
+++ background.js
@@ -3,20 +3,26 @@
 // found in the LICENSE file.
 
 /*
-  Displays a notification with the current time. Requires "notifications"
-  permission in the manifest file (or calling
-  "webkitNotifications.requestPermission" beforehand).
-*/
+ Displays a notification with the current time. Requires "notifications"
+ permission in the manifest file (or calling
+ "webkitNotifications.requestPermission" beforehand).
+ */
 function show() {
   var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
   var hour = time[1] % 12 || 12;               // The prettyprinted hour.
   var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
-  var notification = window.webkitNotifications.createNotification(
-    '48.png',                      // The image.
-    hour + time[2] + ' ' + period, // The title.
-    'Time to make the toast.'      // The body.
-  );
-  notification.show();
+
+  notification =  new Notification('Title', {
+    /* The notification's icon - For Chrome in Windows, Linux & Chrome OS */
+    icon: '48.png',
+    /* The notification’s subtitle. */
+    body: 'Body',
+    /*
+     The notification’s unique identifier.
+     This prevents duplicate entries from appearing if the user has multiple instances of your website open at once.
+     */
+    tag: 'tag'
+  });
 }
 
 // Conditionally initialize the options.
@@ -26,22 +32,21 @@ if (!localStorage.isInitialized) {
   localStorage.isInitialized = true; // The option initialization.
 }
 
-// Test for notification support.
-if (window.webkitNotifications) {
-  // While activated, show notifications at the display frequency.
-  if (JSON.parse(localStorage.isActivated)) { show(); }
-
-  var interval = 0; // The display interval, in minutes.
-
-  setInterval(function() {
-    interval++;
-
-    if (
-      JSON.parse(localStorage.isActivated) &&
-        localStorage.frequency <= interval
-    ) {
-      show();
-      interval = 0;
-    }
-  }, 60000);
+// While activated, show notifications at the display frequency.
+if (JSON.parse(localStorage.isActivated)) {
+  show();
 }
+
+var interval = 0; // The display interval, in minutes.
+
+setInterval(function () {
+  interval++;
+
+  if (
+    JSON.parse(localStorage.isActivated) &&
+    localStorage.frequency <= interval
+    ) {
+    show();
+    interval = 0;
+  }
+}, 1000);
