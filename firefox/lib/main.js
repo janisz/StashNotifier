f2fe597c01339a1acd0f84a00f74f5821a5cc559
Tomek Janiszewski <janiszt@gmail.com>
2014-07-24 22:02:35 +0200

    WIP: enable notification and basic option page

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -11,6 +11,35 @@ var button = buttons.ActionButton({
   onClick: handleClick
 });
 
+
+function createPullRequestNotification(pullRequest) {
+  return new Notification(pullRequest.author.user.displayName, {
+    icon: pullRequest.author.user.avatarUrl.split("?")[0],
+    body: pullRequest.title,
+    tag: 'stash_notifier_' + pullRequest.fromRef.latestChangeset
+  }).onclick = function () {
+    window.open(pullRequest.links.self[0].href)
+  };
+}
+
 function handleClick(state) {
+//  createPullRequestNotification({author : { user : {displayName : "Yoda", avatarUrl : "https://s3.amazonaws.com/ksr/avatars/1488070/Yoda.small.jpg?1365456091"}}, title : "Add missing dependency to Force.js", fromRef : {latestChangeset : 112}})
   tabs.open("http://www.mozilla.org/");
 }
+
+var timers = require("sdk/timers");
+var notifications = require("sdk/notifications")
+
+var i=0;
+var timerId = timers.setInterval(function() {
+  notifications.notify({
+    title: "Jabberwocky",
+    text: "'Twas brillig, and the slithy toves",
+    data: "did gyre and gimble in the wabe",
+    onClick: function (data) {
+      console.log(data);
+      // console.log(this.data) would produce the same result.
+    }
+  });
+  if (i>4) timers.clearInterval(timerId);
+}, 5000);
